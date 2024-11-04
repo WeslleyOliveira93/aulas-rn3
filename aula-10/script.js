1cccbd0fb1f6cb395da87d2b987264a31b125e0c
WeslleyOliveira93 <weslley1993oliveira@gmail.com>
2024-11-04 16:47:25 -0300

    aula-10

---
+++ aula-10/script.js
@@ -0,0 +1,10 @@
+let p = true;
+let q = false;
+console.log(p && q);
+console.log(p || q);
+console.log(p && !q);
+console.log(!p && q);
+console.log(!p || q);
+
+console.log( (4*3) > 125 || (6%3 == 1) ||  (90*2*0.5) == (110-20));
+// aritmetco > comparação > lógico 
